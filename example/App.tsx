7925822da2ba33c8dfca117828d20122d5e15028
lazaralex98 <alex2012ro@gmail.com>
2023-04-10 18:24:52 +0400

    feat: make example prettier

--- example/App.tsx
+++ example/App.tsx
@@ -3,31 +3,34 @@ import { useContextMenu, handleContextMenu, ContextMenu } from "../src/index";
 
 export const App = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-center justify-center bg-gray-50">
       <div
-        className="block cursor-context-menu bg-red-400 p-4"
+        className="block cursor-context-menu bg-red-400 p-4 font-bold text-white"
         onContextMenu={(e) => {
           e.preventDefault();
           console.log("haidee");
           handleContextMenu(e, contextMenuOptions);
         }}
       >
-        <p>Hello world!</p>
+        Right click me!
       </div>
-      <ContextMenu className="bg-gray-50" />
+      <ContextMenu
+        className="rounded bg-white"
+        backgroundClassName="bg-black bg-opacity-10"
+      />
     </div>
   );
 };
 
 const contextMenuOptions = [
   <a
-    className="inline-block w-full p-3 hover:bg-gray-200"
+    className="inline-block w-full p-3 text-left hover:bg-gray-100"
     href="https://dextrac.com"
   >
     Go to DexTrac
   </a>,
   <button
-    className="inline-block w-full p-3 hover:bg-gray-200"
+    className="inline-block w-full p-3 text-left hover:bg-gray-100"
     onClick={() => {
       navigator.clipboard.writeText("Hello world!");
     }}
