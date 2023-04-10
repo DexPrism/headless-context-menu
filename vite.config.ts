82170ed209b0203dd33781b8cf465e20957157a8
lazaralex98 <alex2012ro@gmail.com>
2023-04-10 11:24:34 +0400

    chore: add vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.tsx",
+      name: "HeadlessContextMenu",
+      fileName: (format) => `headless-context-menu.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+});
