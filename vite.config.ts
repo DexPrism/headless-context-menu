deb4fe8e0b059d88b9eff7ffe3b4c51e0fddbe47
lazaralex98 <alex2012ro@gmail.com>
2023-04-12 19:07:11 +0300

    fix: vite config
    An error occured because the build output is in ES module format, but the consuming project's build system (most likely Webpack) expects a CommonJS module.
    
    We adjust the package to provide both CommonJS and ES module builds to fix this issue.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: "src/index.tsx",
+      entry: path.resolve(__dirname, "src/index.tsx"),
       name: "HeadlessContextMenu",
-      fileName: (format) => `headless-context-menu.${format}.js`,
+      formats: ["cjs", "es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: [
+        "react",
+        "react-dom",
+        "zustand",
+        "tailwindcss",
+        "@headlessui/react",
+      ],
+      output: {
+        exports: "named",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          zustand: "zustand",
+          tailwindcss: "tailwindcss",
+          "@headlessui/react": "headlessUI",
+        },
+      },
     },
-  },
-  server: {
-    open: "/example",
   },
 });
